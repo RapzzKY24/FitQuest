19e3383615e0c08fe3bb972d815634fb0ecf0157
RapzzKY24 <paymeriskiaulia@gmail.com>
2026-03-10 23:14:42 +0700

    feat:create onboarding flow page

---
+++ src/app/auth/register/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import { OnboardingFlow } from "@/src/features/auth";
+
+const OnboardingFlowPage = () => {
+  return (
+    <div>
+      <OnboardingFlow />
+    </div>
+  );
+};
+
+export default OnboardingFlowPage;
