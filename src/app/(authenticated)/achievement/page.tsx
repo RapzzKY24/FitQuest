d61af39f05ab7cbe2d23d018888c1bf8b62228d2
RapzzKY24 <paymeriskiaulia@gmail.com>
2026-03-13 00:54:56 +0700

    feat: implement achievement  page from design

---
+++ src/app/(authenticated)/achievement/page.tsx
@@ -0,0 +1,8 @@
+import AchievementPages from "@/src/features/achievement/pages/AchievementPages";
+import React from "react";
+
+const page = () => {
+  return <AchievementPages />;
+};
+
+export default page;
