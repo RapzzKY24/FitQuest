e89a6ca91c6a701fb956ad162330be4caf14c59a
RapzzKY24 <paymeriskiaulia@gmail.com>
2026-03-19 15:12:12 +0700

    feat: add skeleton loading in dashboard page

--- src/features/dashboard/pages/DashboardPages.tsx
+++ src/features/dashboard/pages/DashboardPages.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import HeaderDashboard from "../components/HeaderDashboard";
 import StatsDashboard from "../components/StatsDashboard";
 import DailyQuest from "../components/DailyQuest";
@@ -8,29 +8,58 @@ import GymbroWidget from "../components/GymbroWidget";
 import LeaderboardWidget from "../components/LeaderboardWidget";
 import LevelProgressWidget from "../components/LevelProgresWidget";
 import WeeklyActivity from "../components/WeeklyActivity";
+import { Skeleton, SkeletonCard } from "@/src/components/ui/Skeleton";
 
 const DashboardPages = () => {
   return (
     <main className="flex flex-col gap-6 p-6">
-      <HeaderDashboard />
+      <Suspense fallback={<Skeleton height={100} />}>
+        <HeaderDashboard />
+      </Suspense>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full">
-        <StatsDashboard />
+        <Suspense
+          fallback={
+            <>
+              <Skeleton height={120} />
+              <Skeleton height={120} />
+              <Skeleton height={120} />
+              <Skeleton height={120} />
+            </>
+          }
+        >
+          <StatsDashboard />
+        </Suspense>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 w-full">
         <div className="lg:col-span-3 grid grid-cols-1 xl:grid-cols-2 gap-6">
           <div className="flex flex-col gap-6">
-            <WeeklyActivity />
-            <WorkoutStats />
+            <Suspense fallback={<Skeleton height={300} />}>
+              <WeeklyActivity />
+            </Suspense>
+            <Suspense fallback={<Skeleton height={250} />}>
+              <WorkoutStats />
+            </Suspense>
           </div>
+
           <div className="flex flex-col gap-6">
-            <DailyQuest />
-            <LatestAchievements />
+            <Suspense fallback={<Skeleton height={250} />}>
+              <DailyQuest />
+            </Suspense>
+            <Suspense fallback={<Skeleton height={200} />}>
+              <LatestAchievements />
+            </Suspense>
           </div>
         </div>
         <div className="lg:col-span-1 flex flex-col gap-6">
-          <GymbroWidget />
-          <LevelProgressWidget />
-          <LeaderboardWidget />
+          <Suspense fallback={<Skeleton height={320} />}>
+            <GymbroWidget />
+          </Suspense>
+          <Suspense fallback={<Skeleton height={150} />}>
+            <LevelProgressWidget />
+          </Suspense>
+          <Suspense fallback={<Skeleton height={400} />}>
+            <LeaderboardWidget />
+          </Suspense>
         </div>
       </div>
     </main>
