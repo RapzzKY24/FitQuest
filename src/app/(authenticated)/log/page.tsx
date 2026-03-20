038d4e4a09deb28d7295d909010bc24291c58f8c
RapzzKY24 <paymeriskiaulia@gmail.com>
2026-03-20 09:47:57 +0700

    feat: add LogWorkoutLoading component and implement loading state in LogWorkoutPage

--- src/app/(authenticated)/log/page.tsx
+++ src/app/(authenticated)/log/page.tsx
@@ -1,13 +1,17 @@
-import LogWorkoutPages from "@/src/features/logWorkoutDashboard/pages/LogWorkout";
 import { constructMetadata } from "@/src/utils/metadata";
+import { Suspense } from "react";
+import { LogWorkoutLoading } from "@/src/features/logWorkoutDashboard/components/LogWorkoutLoading";
+import LogWorkoutPages from "@/src/features/logWorkoutDashboard/pages/LogWorkout";
 
 export const metadata = constructMetadata({
   title: "Log Workout",
   description: "Lihat progres latihan harianmu dan klaim XP hari ini!",
 });
 
-const LogWorkout = () => {
-  return <LogWorkoutPages />;
-};
-
-export default LogWorkout;
+export default function LogWorkoutPage() {
+  return (
+    <Suspense fallback={<LogWorkoutLoading />}>
+      <LogWorkoutPages />
+    </Suspense>
+  );
+}
